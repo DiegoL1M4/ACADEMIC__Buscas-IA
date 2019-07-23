10a531371865fa85d4710fa25e5cf1b2e990c14f
Diego Lima <30624298+DiegoL1M4@users.noreply.github.com>
2019-07-23 09:23:16 -0300

    Estruturas de Dados

---
+++ src/EstruturadeDados/Arvore.java
@@ -0,0 +1,21 @@
+package EstruturadeDados;
+
+public class Arvore<tipo> {
+	
+	No<tipo> raiz = new No<tipo>(null,null,0);
+	
+	public Arvore(tipo elemento) {
+		raiz.setElemento(elemento);
+	}
+	
+	public No<tipo> inserirNo(tipo elemento, No<tipo> pai) {
+		No<tipo> node = new No<tipo>(elemento,pai,0);
+		pai.inserir(node); // inserindo nos filhos
+		
+		return node;
+	}
+	
+	public tipo getRaiz() {
+		return raiz.getElemento();
+	}
+}
---
+++ src/EstruturadeDados/Pilha.java
@@ -0,0 +1,54 @@
+package EstruturadeDados;
+
+
+public class Pilha<tipo> {
+	private No<tipo> topo;
+	private int tamanho;
+	
+	public Pilha() {
+		topo = null;
+		tamanho = 0;
+	}
+	
+	public void insere(tipo elemento) {
+		if(topo == null) {
+			topo = new No<tipo>(elemento, null);
+		} else {
+			No<tipo> aux = topo;
+			topo = new No<tipo>(elemento,aux);
+		}
+		tamanho++;
+	}
+	
+	public tipo remove() {
+		if(topo == null)
+			return null;
+		
+		No<tipo> aux = topo;
+		
+		topo = topo.getProximo();
+		
+		aux.setProximo(null);
+		tamanho--;
+		
+		return aux.getElemento();
+	}
+	
+	public void mostrarPilha() {
+		recursaoMostraPilha(topo);
+		System.out.println();
+	}
+	
+	public int recursaoMostraPilha(No<tipo> no) {
+		if (no == null)
+			return 0;
+		recursaoMostraPilha(no.getProximo());
+		System.out.print(no.getElemento() + "\t");
+		return 1;
+	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+	
+}
